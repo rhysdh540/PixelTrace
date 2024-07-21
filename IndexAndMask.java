a0d2f523a7b8f0a553eb1abc6e4b797d4465dcbf
Joey Sodergren <jas.elgoog@gmail.com>
2024-07-21 16:47:56 -0400

    Added IndexAndMask

---
+++ IndexAndMask.java
@@ -0,0 +1,9 @@
+public class IndexAndMask {
+    public final int index;
+    public final long mask;
+
+    public IndexAndMask(int new_index, long new_mask){
+        index = new_index;
+        mask = new_mask;
+    }
+}
