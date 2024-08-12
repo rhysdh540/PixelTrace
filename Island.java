4a931f90e84841d15aca6befa2823673934efbc9
Joey Sodergren <jas.elgoog@gmail.com>
2024-08-12 16:45:17 -0400

    Increase unmodifiability

--- Island.java
+++ Island.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
 public class Island {
-    static private final List<TreeMap<Integer, Integer>> cornerTable = List.of(
+    static private final List<Map<Integer, Integer>> cornerTable = List.of(
         //Index 0 = Right
-        new TreeMap<Integer, Integer>(Map.of(2,1,8,3,13,1,9,3,6,1,7,3)),
+        Map.of(2,1,8,3,13,1,9,3,6,1,7,3),
         //Index 1 = Down
-        new TreeMap<Integer, Integer>(Map.of(8,2,4,0,9,2,6,0,7,2,11,0)),
+        Map.of(8,2,4,0,9,2,6,0,7,2,11,0),
         //Index 2 = Left
-        new TreeMap<Integer, Integer>(Map.of(1,1,4,3,14,1,9,1,6,3,11,3)),
+        Map.of(1,1,4,3,14,1,9,1,6,3,11,3),
         //Index 3 = Up
-        new TreeMap<Integer, Integer>(Map.of(1,0,2,2,14,0,13,2,9,0,6,2))
+        Map.of(1,0,2,2,14,0,13,2,9,0,6,2)
     );
 
     private int global_x_min;
