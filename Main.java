6c2ba43ce4eb8b714c0b5b4ae0362c2408cc0a1c
Joey Sodergren <jas.elgoog@gmail.com>
2024-08-13 14:15:10 -0400

    Consolidating image scan loops

--- Main.java
+++ Main.java
@@ -10,48 +10,38 @@ class Main{
         return sb.toString();
     }
 
-    private static int[] countColors(BufferedImage bitmap){
-        HashSet<Integer> colors = new HashSet<>();
-        final int width = bitmap.getWidth();
-        final int height = bitmap.getHeight();
-        for(int y=0; y<height; y++){
-            for(int x=0; x<width; x++){
-                colors.add(bitmap.getRGB(x, y));
-            }
-        }
-        System.out.println(colors.size() + " distinct colors.");
-        return colors.stream().mapToInt(i->i).toArray();
-    }
-
     private static ColorLayer[] createLayers(BufferedImage bitmap){
         final int width = bitmap.getWidth();
         final int height = bitmap.getHeight();
-        int[] colors = countColors(bitmap);
         HashMap<Integer, ArrayList<IntPoint>> detections = new HashMap<>();
-        for(int c : colors){
-            detections.put(c, new ArrayList<>());
-        }
         for(int y=0; y<height; y++){
             for(int x=0; x<width; x++){
-                detections.get(bitmap.getRGB(x, y)).add(new IntPoint(x, y));
+                int color = bitmap.getRGB(x, y);
+                if(detections.containsKey(color)){
+                    detections.get(color).add(new IntPoint(x, y));
+                } else {
+                    ArrayList<IntPoint> newList = new ArrayList<>();
+                    newList.add(new IntPoint(x, y));
+                    detections.put(color, newList);
+                }
             }
         }
-        ColorLayer[] layers = new ColorLayer[colors.length];
-        for(int i=0; i<colors.length; i++){
-            if(i % 100 == 0){
-                System.out.print(i + " ColorLayers created.\r");
+        ColorLayer[] layers = new ColorLayer[detections.size()];
+        int counter = 0;
+        for(int color : detections.keySet()){
+            if(counter % 100 == 0){
+                System.out.print(counter + " ColorLayers created.\r");
                 System.out.flush();
             }
-            layers[i] = new ColorLayer(colors[i], detections.get(colors[i]));
-            detections.remove(colors[i]);
+            layers[counter] = new ColorLayer(color, detections.get(color));
+            counter++;
         }
-        System.out.println(layers.length + " ColorLayers created.");
+        System.out.println(counter + " ColorLayers created.");
         return layers;
     }
     public static void main(String[] args) throws Exception{
         final long startTime = System.currentTimeMillis();
-        BufferedImage original = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        original = ImageIO.read(new File("TestBitmaps/WeezerSmall.png"));
+        BufferedImage original = ImageIO.read(new File("TestBitmaps/WeezerSmall.png"));
         final int width = original.getWidth();
         final int height = original.getHeight();
         ColorLayer[] layers = createLayers(original);
